b34c1e1a32e2c4fb25cb1518c0e59019e020a165
123aleqsandre <147233725+123aleqsandre@users.noreply.github.com>
2026-05-27 20:34:48 +0400

    cards

--- OctarinoxNewGen/src/pages/home.jsx
+++ OctarinoxNewGen/src/pages/home.jsx
@@ -1,5 +1,8 @@
+import OpenSourceCards from "../component/card";
+
 export default function Home(){
     return <>
+    <OpenSourceCards/>
     <h1 className="text-4xl font-bold text-center mt-8">Welcome to Octarinox</h1>
     <p className="text-center mt-4 text-lg">Your one-stop solution for all your needs.</p>
     </>
