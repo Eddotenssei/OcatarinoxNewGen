d5fe65c46f5e5d154f6a9d2bc4e9e8ccf1663e2c
Ilia Chichikoshvili <eddotenssei@gmail.com>
2026-05-29 21:47:21 +0400

    zogacu cardi
    zogadi cardi gavakete sadac shegvidzlia rom proplebad gadavgzavnot tiutle da textiu

--- OctarinoxNewGen/src/pages/aboutUs.jsx
+++ OctarinoxNewGen/src/pages/aboutUs.jsx
@@ -1,7 +1,9 @@
+import Card from "../component/card";
+
 export default function AboutUS(){
     return (
-        <h1>about us
-            
+        <h1>
+            <Card title="About us" text="ragaca about usze"/>
         </h1>
     )
 }
--- OctarinoxNewGen/src/pages/contact.jsx
+++ OctarinoxNewGen/src/pages/contact.jsx
@@ -1,5 +1,9 @@
+import Card from "../component/card";
+
 export default function Contact(){
     return(
-        <h1>contact</h1>
+       <h1>
+        <Card title="Contact Us" text="ragaca about contactze"/>
+    </h1>
     )
 }
